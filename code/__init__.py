57075a8e7b1b67d5ae0a602bb438c182570cf715
Krzysztof Orliński <59778570+Arghantyr@users.noreply.github.com>
2020-06-27 11:36:09 +0200

    Add __init__.py

---
+++ code/__init__.py
@@ -0,0 +1,6 @@
+from .OpticalMaterial import OpticalMaterial
+from .Nanoparticle import Nanoparticle
+from .Sellmeier import Sellmeier
+from .SpectralMeasurement import SpectralMeasurement
+
+__all__ = ["OpticalMaterial","Nanoparticle","Sellmeier","SpectralMeasurement"]
