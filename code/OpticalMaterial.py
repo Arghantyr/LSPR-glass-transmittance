9e3f124ddfe6806decfd2fe74f0b0d97197df93f
Krzysztof Orliński <59778570+Arghantyr@users.noreply.github.com>
2023-11-02 21:17:45 +0100

    Update OpticalMaterial.py
    Small fixes

--- code/OpticalMaterial.py
+++ code/OpticalMaterial.py
@@ -23,8 +23,8 @@ class OpticalMaterial:
         import numpy as np
         
         # Handle the name
-        if type(name) == str:
-            if name[-4:] == ".csv":
+        if isinstance(name, str):
+            if name.endswith(".csv"):
                 raise NameError("Invalid type for the 'name'. Must be 'str'.")
             else:
                 self.name = name
@@ -33,13 +33,13 @@ class OpticalMaterial:
 
 
         # Handle the source
-        if type(source) == str:
-            if source[-4:] == '.csv':
+        if isinstance(source, str):
+            if source.endswith('.csv'):
                 self.refractive_index = pd.read_csv(source)
                 self.refractive_index.columns = ["Wavelength", "n", "k"]
             else:
                 raise Exception("Invalid sourcefile. Only '.csv' or 'pd.DataFrame' type accepted.")
-        elif type(source) == pd.DataFrame:
+        elif isinstance(source, pd.DataFrame):
             self.refractive_index = source
             self.refractive_index.columns = ["Wavelength", "n", "k"]
         else:
@@ -49,18 +49,17 @@ class OpticalMaterial:
         self.n = p.drop(columns=['k'])
         self.k = p.drop(columns=['n'])
 
-        self.permittivity = pd.concat([p.iloc[:,0],
-                                       p.iloc[:,1]**2 - p.iloc[:,2]**2,
+        self.permittivity = pd.concat([p.iloc[:,0], p.iloc[:,1]**2 - p.iloc[:,2]**2,
                                        2*p.iloc[:,1] * p.iloc[:,2]],
-                                      axis=1, keys=[p.columns[0],
-                         "Real permittivity, a.u.",
-                         "Imaginary permittivity, a.u."])
+                                      axis=1,
+                                      keys=[p.columns[0],
+                                            "Real permittivity, a.u.",
+                                            "Imaginary permittivity, a.u."])
         self.real_permittivity = self.permittivity.drop(columns=["Imaginary permittivity, a.u."])
         self.imaginary_permittivity = self.permittivity.drop(columns=["Real permittivity, a.u."])
 
     def even_xspacing(self, optical_property, left_bound: float = None,
                       right_bound: float = None, spacing: float = None):
-
         """
         Takes optical_property, e.g. refractive_index and returns evenly spaced data with defined spacing.
         Using spacing smaller than average spacing in the original data with throw a warning.
@@ -84,20 +83,22 @@ class OpticalMaterial:
         lds = len(ds)
         spacings = [ds.iloc[k+1]-ds.iloc[k] for k in range(lds-1)]
         avg_spacing = sum(spacings) / len(spacings)
-        x_new = pd.Series(np.arange(minds, maxds, spacing), name="Wavelength")
+        x_new = pd.Series(np.arange(minds, maxds, spacing),
+                          name="Wavelength")
         x_additional = pd.merge(ds, x_new, how="outer").sort_values(by="Wavelength").reset_index(drop=True)
 
         # Warn if new spacing is smaller than old spacing
         len_spacing = len(str(spacing))
         if spacing < avg_spacing:
-            print(f"Warning: the specified value of spacing {spacing} is smaller than the average",
-                  f" value of the spacing from the dataset {avg_spacing}. Interpolation may cause errors,",
-                 f" depending on the difference between the new spacing and the average old spacing")
+            print(f"""Warning: the specified value of spacing {spacing} is smaller than the average
+            value of the spacing from the dataset {avg_spacing}. Interpolation may cause errors,
+            depending on the difference between the new spacing and the average old spacing""")
 
 
         # Preprocess the data
         # add values separated by new spacing, with NaN as missing values if the value is not there
-        new_x = pd.DataFrame(x_additional, columns=['Wavelength'])
+        new_x = pd.DataFrame(x_additional,
+                             columns=['Wavelength'])
         new_empty = pd.DataFrame(columns=list(p.columns[-2:])).fillna(np.nan)
         reg_points = pd.concat([new_x, new_empty], axis=0, keys=list(p.columns))
 
@@ -123,4 +124,4 @@ class OpticalMaterial:
         return regspace
 
     def __repr__(self):
-        return "{}".format(self.name)
+        return f"{self.name}"
